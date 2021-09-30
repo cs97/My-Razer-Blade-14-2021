6f90c9804b071097a7fb649cf1420d5753fefdef
leftside97 <chrswr@protonmail.com>
2021-10-01 01:39:33 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install akmod-nvidia
+dnf install xorg-drv-nvidia-cuda
