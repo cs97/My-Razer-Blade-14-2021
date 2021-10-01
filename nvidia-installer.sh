439aeadcd143af239d714a2ffe3a4f62cf59be72
leftside97 <chrswr@protonmail.com>
2021-10-01 12:53:08 +0200

    Update nvidia-installer.sh

--- nvidia-installer.sh
+++ nvidia-installer.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 case $1 in
-	"1") sudo dnf update;;
+	"1") dnf update;;
 	"2") dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig;;
-	"3") dnf remove xorg-x11-drv-nouveau;;
-	"4")	mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
+	"3") echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf;;
+	"4") dnf remove xorg-x11-drv-nouveau;;
+	"5")	mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
 		dracut /boot/initramfs-$(uname -r).img $(uname -r);;
-	"5") systemctl set-default multi-user.target && reboot;;
-	"6") sudo su;;
-	"7") ./NVIDIA-Linux-*.run;;
-	"8") systemctl set-default graphical.target && reboot;;
+	"6") systemctl set-default multi-user.target && reboot;;
+	"7") sudo su;;
+	"8") ./NVIDIA-Linux-*.run;;
+	"9") systemctl set-default graphical.target && reboot;;
 	*) cat $0;;
 esac
 
