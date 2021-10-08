ac8da5af620360d44a340758f3bc64141b6bbc1e
leftside97 <chrswr@protonmail.com>
2021-10-08 22:32:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+
+#fedora
+
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 dnf install akmod-nvidia
 dnf install xorg-x11-drv-nvidia-cuda
 
-dnf install arc-theme pip Virtualbox simplescreenrecorder htop
+dnf install arc-theme pip Virtualbox simplescreenrecorder htop nvtop radeontop
