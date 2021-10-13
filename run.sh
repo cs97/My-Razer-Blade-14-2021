9f8e8deb3d408e2355afc2c202b8f6077e5c4d46
leftside97 <chrswr@protonmail.com>
2021-10-13 11:43:18 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 #fedora
 
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 dnf install akmod-nvidia
 dnf install xorg-x11-drv-nvidia-cuda
