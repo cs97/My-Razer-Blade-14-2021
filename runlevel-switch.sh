3c34e805095e73493d267e37633f6a864ea7dded
cs97 <chrswr97@gmail.com>
2022-10-27 19:39:55 +0200

    Delete runlevel-switch.sh

--- runlevel-switch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-case $1 in
-	"-m") systemctl set-default multi-user.target && reboot;;
-	"-g") systemctl set-default graphical.target && reboot;;
-	*) echo "./runlevel-switch.sh <-m|-g>";;
-esac
